309022c2fd64f67a71cce415c8b75a72450d398e
alice <minion.banana.poteto2013@gmail.com>
2023-10-03 07:35:39 +0900

    スライダーを追加

--- text_to_speech.py
+++ text_to_speech.py
@@ -28,18 +28,23 @@ thread = None
 
 # レイアウトの定義
 layout = [
-    [sg.Text("回数: "), sg.InputText(key='count', default_text='1')],
-    [sg.Text("読み上げるテキスト: "), sg.Text('', size=(30, 1), key='output')],
-    [sg.Button('実行'), sg.Button('停止'), sg.Button('終了')],
+    [sg.Text("回数 ", size=(12, 1)), sg.Slider(range=(1, 10), orientation='h', size=(15, 20), key='count_slider', default_value=1)],
+    [sg.Text("待機時間（秒）: ", size=(12, 1)), sg.Slider(range=(1, 10), orientation='h', size=(15, 20), key='slider', default_value=5)],
+    [sg.Text("実行状況 ", size=(12, 1)), sg.ProgressBar(100, orientation='h', size=(10, 20), key='progressbar'),
+     sg.Text('', size=(12, 1), key='progress_text')],
+    [sg.Text("読み上げるテキスト: ", size=(15, 1)), sg.Text('', size=(30, 1), key='output')],
+    [sg.Button('実行', key='execute_button'), sg.Button('停止'), sg.Button('終了')],
 ]
 
 # ウィンドウの生成
 window = sg.Window('テキスト読み上げアプリ', layout)
 
 
-def read_aloud(count):
+def read_aloud(count, sleep_duration):
     global thread
-    for _ in range(int(count)):
+    progress_bar = window['progressbar']
+    progress_text = window['progress_text']
+    for i in range(int(count)):
         if thread is not None and not thread.is_alive():
             break  # スレッドが中断された場合はループを終了
 
@@ -48,7 +53,6 @@ def read_aloud(count):
         # テキストを音声に変換
         tts = gTTS(text=dajare, lang='ja')
         tts.save('dajare.mp3')
-
         # 音声を再生
         playsound('dajare.mp3')
 
@@ -57,18 +61,28 @@ def read_aloud(count):
         if file_to_delete.exists():
             file_to_delete.unlink()
 
-        # 次のダジャレまで待機（テキストボックスからの秒数を使用）
-        time.sleep(5)
+        # 次のダジャレまで待機
+        time.sleep(sleep_duration)
+
+        # プログレスバーを更新
+        progress_value = int(((i + 1) / int(count)) * 100)
+        progress_bar.update(progress_value)
+        progress_text.update(f'{progress_value}% 完了')
+
+    # 実行が終了したらスレッドをクリア
+    thread = None
 
 
 # ボタンのイベントハンドラ
 def button_handler(event):
     global thread
-    if event == '実行':
-        count = window['count'].get()
+    if event == 'execute_button':
+        event, values = window.read()
+        count = int(values['count_slider'])
+        sleep_duration = int(values['slider'])
         # スレッドが実行中でない場合にスレッドを開始
         if thread is None or not thread.is_alive():
-            thread = threading.Thread(target=read_aloud, args=(count,))
+            thread = threading.Thread(target=read_aloud, args=(count, sleep_duration))
             thread.start()
     elif event == '停止':
         # スレッドを中断
