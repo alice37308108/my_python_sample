db92b4185911e1809040c664c29bc0cf97028b69
alice <minion.banana.poteto2013@gmail.com>
2023-10-03 22:45:53 +0900

    text_to_speech.pyを修正

--- text_to_speech.py
+++ text_to_speech.py
@@ -26,16 +26,27 @@ dajare_list = [
 # スレッドを管理する変数
 thread = None
 
+sg.theme('LightGray2')
+
 # レイアウトの定義
 layout = [
-    [sg.Text("回数 ", size=(12, 1)), sg.Slider(range=(1, 10), orientation='h', size=(15, 20), key='count_slider', default_value=1)],
-    [sg.Text("待機時間（秒）: ", size=(12, 1)), sg.Slider(range=(1, 10), orientation='h', size=(15, 20), key='slider', default_value=5)],
-    [sg.Text("実行状況 ", size=(12, 1)), sg.ProgressBar(100, orientation='h', size=(10, 20), key='progressbar'),
+    [sg.Text("回数 ", size=(12, 1)),
+     sg.Slider(range=(1, 10), orientation='h', size=(30, 20), key='count_slider', default_value=1)],
+    [sg.Text("待機時間（秒）: ", size=(12, 1)),
+     sg.Slider(range=(1, 10), orientation='h', size=(30, 20), key='slider', default_value=5)],
+    [sg.Text("実行状況 ", size=(12, 1)), sg.ProgressBar(100, orientation='h', size=(20, 20), key='progressbar'),
      sg.Text('', size=(12, 1), key='progress_text')],
-    [sg.Text("読み上げるテキスト: ", size=(15, 1)), sg.Text('', size=(30, 1), key='output')],
     [sg.Button('実行', key='execute_button'), sg.Button('停止'), sg.Button('終了')],
+    [sg.Text('_' * 70)],  # 線を引く
+    [
+        sg.Text('', size=(15, 1)),
+        sg.Text('', size=(30, 1), key='output'),
+        sg.Column([
+            [sg.Text('', size=(30, 1), key='output')],
+            [sg.Image(filename='cthulhu_deep_ones.png', key='image')],
+        ], justification='right')
+    ],
 ]
-
 # ウィンドウの生成
 window = sg.Window('テキスト読み上げアプリ', layout)
 
@@ -77,13 +88,12 @@ def read_aloud(count, sleep_duration):
 def button_handler(event):
     global thread
     if event == 'execute_button':
-        event, values = window.read()
-        count = int(values['count_slider'])
-        sleep_duration = int(values['slider'])
-        # スレッドが実行中でない場合にスレッドを開始
-        if thread is None or not thread.is_alive():
-            thread = threading.Thread(target=read_aloud, args=(count, sleep_duration))
-            thread.start()
+        count = window['count_slider'].TKScale.get()
+        sleep_duration = window['slider'].TKScale.get()
+
+        # スレッドを開始
+        thread = threading.Thread(target=read_aloud, args=(count, sleep_duration))
+        thread.start()
     elif event == '停止':
         # スレッドを中断
         if thread is not None and thread.is_alive():
